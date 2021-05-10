d54acb33f4ba496d2888b1f7ccf2fca83164caa3
Htmil <82089360+Htmil@users.noreply.github.com>
2021-05-10 10:40:18 +0200

    UI Design
    detta är ui delen...

--- Mappstruktur/php/page_php/dialogue.php
+++ Mappstruktur/php/page_php/dialogue.php
@@ -1,16 +1,25 @@
 <a class="viewLink left" href="#map">Karta</a>
 <a class="viewLink" href="#inventory">Samling</a>
-<a class="viewLink" href="../functional_php/admin/logout.php">Logga ut</a>
+<a class="viewLink histBtn">Historik</a>
 <a class="viewLink right" href="#interactive">Spel/Gåta</a>
+<a class="viewLink logoutBtn" href="../functional_php/admin/logout.php">Logga ut</a>
+
+
 <link rel="stylesheet" href="../../css/dialogue.css">
-<div class="menuBG">
+<div id="overlayStandby">
+    <img src="../../media/illustrations/malmu_icon.png" alt="Malmu">
+    <img src="../../media/illustrations/searching.gif" alt="Letar efter kontakt..">
+        <div class="textlayer">
+            <h2>Söker Uppkoppling...</h2>
+            <p>Ta dig till en anslutningsplats</p>
+        </div>
 </div>
-<div class="flexer">
+<div class="flexer none">
     <div class="none" id="dialogueWrapper">
-        <img id="characterImg">
+        <img src="../../media/illustrations/Ledsna.png" id="characterImg">
         <div id="dialogueBox">
-            <div id="characterName"></div>
-            <p></p>
+            <div id="characterName">Ledsna Gubben</div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quidem voluptatibus natus architecto amet quas in</p>
             <div class="none" id="indicator"></div>
         </div>
     </div>
