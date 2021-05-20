963928c813896a260f7b64dc3d7bdca98855cfe3
LudvigStridsberg <77850704+LudvigStridsberg@users.noreply.github.com>
2021-05-20 22:41:48 +0200

    Added end-pages
    Added two new pages that the user is sent to after they've read their ending.

--- Mappstruktur/js/endgame.js
+++ Mappstruktur/js/endgame.js
@@ -9,18 +9,44 @@ function endgame() {
     const imgElement = document.querySelector("#overlayStandby > img:nth-of-type(2)");
     const textOverlay = document.querySelector(".textlayer");
 
+    // Create the countdown-element
     let countdown = document.createElement("p");
     countdown.id = "countdown";
+
+    let timeleft = 15;
+    let countdownTimer = setInterval(function(){
+
+    // Changes the color of the countdown element in steps from green->yellow->red, 0 == removes counter.   
+        if(timeleft <= 15) {
+            document.getElementById("countdown").style.color = "#5FBC7C";
+        } if(timeleft <= 10) {
+            document.getElementById("countdown").style.color = "#E7C55F";
+        } if(timeleft <= 5) {
+            document.getElementById("countdown").style.color = "#dd3f3f";
+        } if(timeleft <= 0){
+            clearInterval(countdownTimer);
+            document.getElementById("countdown").classList.add("none");
+            gkBtn.click();
+            parent.classList.remove("flexer");
+            parent.classList.add("none");
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft;
+        }
+        timeleft -= 1;
+    }, 1000);
     
+    // Set the text of the buttons
     gkBtn.innerText = "Portmästaren";
     adelBtn.innerText = "Ädeln";
 
+    // Remove the old buttons
     oldBtns.forEach(button => {
         button.classList.add("none");
     });
 
     adelBtn.addEventListener("click", function() {
         endgameChoice = "outroB";
+        clearInterval(countdownTimer);
         patchState("currentUser", "completedGame", true);
         setTimeout(() => {
             dialogueInit();
@@ -29,6 +55,7 @@ function endgame() {
     
     gkBtn.addEventListener("click", function() {
         endgameChoice = "outroA";
+        clearInterval(countdownTimer);
         patchState("currentUser", "completedGame", true);
         setTimeout(() => {
             dialogueInit();
@@ -54,25 +81,4 @@ function endgame() {
     // Change the style of the wrapper #mainBtnNav
     parent.style.bottom = "17px";
 
-    var timeleft = 15;
-    var downloadTimer = setInterval(function(){
-
-    // Changes the color of the countdown element in steps from green->yellow->red, 0 == removes counter.   
-        if(timeleft <= 15) {
-            document.getElementById("countdown").style.color = "#5FBC7C";
-        } if(timeleft <= 10) {
-            document.getElementById("countdown").style.color = "#E7C55F";
-        } if(timeleft <= 5) {
-            document.getElementById("countdown").style.color = "#dd3f3f";
-        } if(timeleft <= 0){
-            clearInterval(downloadTimer);
-            document.getElementById("countdown").classList.add("none");
-            gkBtn.click();
-            parent.classList.remove("flexer");
-            parent.classList.add("none");
-        } else {
-            document.getElementById("countdown").innerHTML = timeleft;
-        }
-        timeleft -= 1;
-    }, 1000);
 }
